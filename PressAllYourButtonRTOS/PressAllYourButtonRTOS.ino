a13681024cae60419f8f44b8e6e6200e7831917e
unknown <pqcamh228@gmail.com>
2022-02-07 01:24:59 +0800

    20220207 We can now update wifi info without re-upload

--- PressAllYourButtonRTOS/PressAllYourButtonRTOS.ino
+++ PressAllYourButtonRTOS/PressAllYourButtonRTOS.ino
@@ -8,12 +8,14 @@
 #define LED_BUILTIN 5
 #endif
 
+#include "EEPROM.h"
+
 // -----Neopixels-----
 #include <FastLED.h>
 #define NUM_LEDS 16
 #define Led_DataPin 32
 CRGBArray<NUM_LEDS + 1> leds;
-CRGB ledsColor = CRGB(100,0,0);
+CRGB ledsColor = CRGB(100, 0, 0);
 int ledcycle = 0;
 
 // -----Touches-----
@@ -21,6 +23,15 @@ int ledcycle = 0;
 int laststate = 1;
 // ----------------------Conectivity---------------------------------
 #include <WiFi.h>
+#include <WebServer.h>
+#include "Pages.h"       // The wifi setting page
+
+
+WebServer server(80);
+// Wifi_setter.h needs to be include after WebServer server(80)
+// because it has dependency on it
+#include "Wifi_setter.h" 
+
 #include "AzureIotHub.h"
 #include "Esp32MQTTClient.h"
 
@@ -32,8 +43,8 @@ int laststate = 1;
 bool isWifiOn = false;
 
 // Please input the SSID and password of WiFi
-const char *ssid = "7H4F";
-const char *password = "16881688";
+char ssid[100];
+char password[100];
 
 /*String containing Hostname, Device Id & Device Key in the format:                         */
 /*  "HostName=<host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"                */
@@ -63,48 +74,74 @@ void setup()
   // --------------------------Manual Setup------------------------------
   // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
+  EEPROM.begin(400);
 
   Serial.println("ESP32 Device");
   Serial.println("Initializing...");
   // Neopixels Init
   FastLED.addLeds<NEOPIXEL, Led_DataPin>(leds, NUM_LEDS);
-	FastLED.clear();
-	FastLED.show();
-  
+  FastLED.clear();
+  FastLED.show();
+
   // touchbutton
   pinMode(touchOutput_pin, INPUT);
-  
+
   delay(3000);
 
   // Check Wifi bypass pin
   pinMode(WIFI_BYPASS_PIN, INPUT);
-  if(digitalRead(WIFI_BYPASS_PIN) == 1)
+  if (digitalRead(WIFI_BYPASS_PIN) == 1)
   {
     isWifiOn = true;
   }
 
-  if(isWifiOn == true)
+  // Check wifi credential
+  if (isWifiOn)
   {
-      // Initialize the WiFi module
-      Serial.println(" > WiFi");
-      hasWifi = false;
-      InitWifi();
-      if (!hasWifi)
-      {
-        return;
-      }
-      randomSeed(analogRead(0));
-      
-      Serial.println(" > IoT Hub");
-      Esp32MQTTClient_SetOption(OPTION_MINI_SOLUTION_NAME, "GetStarted");
-      Esp32MQTTClient_Init((const uint8_t *)connectionString, true);
+    if (!CheckWIFICreds())
+    {
+      Serial.println("No WIFI credentials stored in memory. Loading form...");
+      while (loadWIFICredsForm())
+        ;
+    }
 
-      Esp32MQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
-      Esp32MQTTClient_SetMessageCallback(MessageCallback);
-      Esp32MQTTClient_SetDeviceTwinCallback(DeviceTwinCallback);
-      Esp32MQTTClient_SetDeviceMethodCallback(DeviceMethodCallback);
+    String s = EEPROM.readString(100);
+    String p = EEPROM.readString(200);
+    // copy the info from eeporom to our char array using help of strcpy and c_str
+    strcpy(ssid, s.c_str());
+    strcpy(password, p.c_str());
+    
+    
+    Serial.println("Successfully set wifi info from eeprom");
+   
 
-      send_interval_ms = millis();
+    delay(2000);
+  }
+
+  if (isWifiOn == true)
+  {
+    // Initialize the WiFi module
+    Serial.println(" > WiFi");
+    hasWifi = false;
+
+    InitWifi();
+    
+    if (!hasWifi)
+    {
+      return;
+    }
+    randomSeed(analogRead(0));
+
+    Serial.println(" > IoT Hub");
+    Esp32MQTTClient_SetOption(OPTION_MINI_SOLUTION_NAME, "GetStarted");
+    Esp32MQTTClient_Init((const uint8_t *)connectionString, true);
+
+    Esp32MQTTClient_SetSendConfirmationCallback(SendConfirmationCallback);
+    Esp32MQTTClient_SetMessageCallback(MessageCallback);
+    Esp32MQTTClient_SetDeviceTwinCallback(DeviceTwinCallback);
+    Esp32MQTTClient_SetDeviceMethodCallback(DeviceMethodCallback);
+
+    send_interval_ms = millis();
   }
   // ||||||||||||||||||||||||||Manual Setup|||||||||||||||||||||||||||||||
   // --------------------------Queues Defination----------------------------
@@ -113,22 +150,22 @@ void setup()
   TaskHandle_t xAzureExampleHandle;
   // --------------------------Task Defination------------------------------
   xTaskCreatePinnedToCore(
-    xTaskTouchButton
-    ,  "TaskTouchButton"   // A name just for humans
-    ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL 
-    ,  ARDUINO_RUNNING_CORE);
+      xTaskTouchButton, "TaskTouchButton" // A name just for humans
+      ,
+      1024 // This stack size can be checked & adjusted by reading the Stack Highwater
+      ,
+      NULL, 2 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+      ,
+      NULL, ARDUINO_RUNNING_CORE);
 
   xTaskCreatePinnedToCore(
-    xTaskLED
-    ,  "TaskLED"   // A name just for humans
-    ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL 
-    ,  ARDUINO_RUNNING_CORE);
+      xTaskLED, "TaskLED" // A name just for humans
+      ,
+      1024 // This stack size can be checked & adjusted by reading the Stack Highwater
+      ,
+      NULL, 2 // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+      ,
+      NULL, ARDUINO_RUNNING_CORE);
 
   xTaskCreatePinnedToCore(
       xTaskAzureExample, "AzureExample", 8096 // Stack size
@@ -137,12 +174,12 @@ void setup()
       ,
       &xAzureExampleHandle, ARDUINO_RUNNING_CORE);
 
-    // --------------------------Suspend task by conditions------------------------------
-    if(isWifiOn == false)
-    {
-      vTaskSuspend(xAzureExampleHandle);
-      Serial.println("Initialed With No WIFI");
-    }
+  // --------------------------Suspend task by conditions------------------------------
+  if (isWifiOn == false)
+  {
+    vTaskSuspend(xAzureExampleHandle);
+    Serial.println("Initialed With No WIFI");
+  }
 }
 // ||||||||||||||||||||||||||||||||Task Defination||||||||||||||||||||||||||||||||||||
 
@@ -233,15 +270,10 @@ void xTaskDealCommand(void *pvParameters)
 {
   for (;;)
   {
-   
+
     vTaskDelay(20);
   }
-
-
 }
-
-
-
 
 void xTaskTouchButton(void *pvParameters)
 {
@@ -249,70 +281,63 @@ void xTaskTouchButton(void *pvParameters)
 
   for (;;)
   {
-    if(digitalRead(touchOutput_pin) == LOW && laststate == 1)
+    if (digitalRead(touchOutput_pin) == LOW && laststate == 1)
     {
-      ledcycle ++;
-      if(ledcycle >= 3)
+      ledcycle++;
+      if (ledcycle >= 3)
         ledcycle = 0;
-      if(ledcycle == 0)
+      if (ledcycle == 0)
       {
         send_led_color[0] = 100;
         send_led_color[1] = 0;
         send_led_color[2] = 0;
       }
-      else if(ledcycle == 1)
+      else if (ledcycle == 1)
       {
         send_led_color[0] = 0;
         send_led_color[1] = 100;
         send_led_color[2] = 0;
       }
-      else if(ledcycle == 2)
+      else if (ledcycle == 2)
       {
         send_led_color[0] = 0;
         send_led_color[1] = 0;
         send_led_color[2] = 100;
       }
-      
 
-
-      if(xQueueSend(xq_to_xTaskLED, send_led_color, 0) == pdTRUE)
+      if (xQueueSend(xq_to_xTaskLED, send_led_color, 0) == pdTRUE)
       {
         Serial.println("sent color to leds");
       }
-      
-      
 
       laststate = 0;
-    }else if(digitalRead(touchOutput_pin) == HIGH && laststate == 0)
+    }
+    else if (digitalRead(touchOutput_pin) == HIGH && laststate == 0)
     {
       laststate = 1;
     }
-    
+
     vTaskDelay(20);
   }
-
-
 }
-
 
 void xTaskLED(void *pvParameters) // This is a task.
 {
   (void)pvParameters;
   uint8_t color[3] = {0};
-  
+
   for (;;)
   {
     if (xQueueReceive(xq_to_xTaskLED, color, portMAX_DELAY) == pdTRUE)
     {
-      for( int i = 0; i < NUM_LEDS; i++) {
+      for (int i = 0; i < NUM_LEDS; i++)
+      {
         leds[i] = CRGB(color[0], color[1], color[2]);
+      }
+      vTaskDelay(200);
+      FastLED.show();
+      vTaskDelay(20);
     }
-    vTaskDelay(200);
-    FastLED.show();
-    vTaskDelay(20);
-
-    }
-    
   }
 }
 
@@ -330,8 +355,8 @@ void xTaskAzureExample(void *pvParameters)
         snprintf(messagePayload, MESSAGE_MAX_LEN, messageData, DEVICE_ID, messageCount++, temperature, humidity);
         Serial.println(messagePayload);
         EVENT_INSTANCE *message = Esp32MQTTClient_Event_Generate(messagePayload, MESSAGE);
-        //Esp32MQTTClient_Event_AddProp(message, "temperatureAlert", "true");
-        //Esp32MQTTClient_SendEventInstance(message);
+        Esp32MQTTClient_Event_AddProp(message, "temperatureAlert", "true");
+        Esp32MQTTClient_SendEventInstance(message);
 
         send_interval_ms = millis();
         Serial.println("message sent!");
@@ -344,6 +369,3 @@ void xTaskAzureExample(void *pvParameters)
     vTaskDelay(10);
   }
 }
-
-
-
